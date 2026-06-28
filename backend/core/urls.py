1de751b5ff1820c78503b3f3b39865b11c797770
Jankiba Mangrola <jankibamangrola11@gmail.com>
2026-06-28 08:23:58 +0530

    remove temp admin endpoint

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,20 +1,9 @@
+# pyrefly: ignore [missing-import]
 from django.contrib import admin
+# pyrefly: ignore [missing-import]
 from django.urls import path, include
-from django.contrib.auth.models import User
-from django.http import JsonResponse
-
-def create_admin(request):
-    if User.objects.filter(username="admin").exists():
-        return JsonResponse({"message": "User already exists"})
-    User.objects.create_superuser(
-        username="admin",
-        email="admin@example.com",
-        password="Admin@12345"
-    )
-    return JsonResponse({"message": "Admin user created"})
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('predictor.urls')),
-    path('create-admin/', create_admin),
 ]
